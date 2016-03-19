537a3749158913cfa1382b2c394e52b5e5ea8218
Joy Nag <joyruet06@gmail.com>
2016-03-19 21:49:00 +1100

    adds left and right turning capability into Direction class

--- lib/direction.rb
+++ lib/direction.rb
@@ -14,6 +14,8 @@ class Direction
   @SOUTH = Direction.new 'SOUTH'
   @WEST  = Direction.new 'WEST'
 
+  @CLOCKWISE_DIRECTIONS = [ @NORTH, @EAST, @SOUTH, @WEST ];
+
   def self.NORTH
     @NORTH
   end
@@ -30,9 +32,16 @@ class Direction
     @WEST
   end
 
-
   def self.[](direction)
     self.instance_variable_get("@#{direction}") if self.instance_variable_defined?("@#{direction}")
+  end
+
+  def self.left(direction)
+    @CLOCKWISE_DIRECTIONS[ ( @CLOCKWISE_DIRECTIONS.index(direction) - 1 ) % 4 ]
+  end
+
+  def self.right(direction)
+    @CLOCKWISE_DIRECTIONS[ ( @CLOCKWISE_DIRECTIONS.index(direction) + 1 ) % 4 ]
   end
 
 end
